48610fa7a3772935e95780d37358a248d2d3c069
abdelrahmanyasser10 <abdelrahmanyasser101112@gmail.com>
2023-08-20 22:57:46 +0300

    created Soldiers Class

---
+++ soldiers.h
@@ -0,0 +1,21 @@
+#ifndef _SOLDIERS_H_
+#define _SOLDIERS_H_
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Soldier{
+protected:
+    string Soldier_Name;
+    string Soldier_Special_Ability;
+    float Soldier_Damage_Value;
+public:
+    virtual void Set_Soldier_Name() = 0;
+    virtual void Set_Soldier_Special_Ability() = 0;
+    virtual void Set_Soldier_Damage_Value() = 0;
+};
+
+
+#endif
