5c5f51dc89365e02754515ab43b2965aafef1ea5
abdelrahmanyasser10 <abdelrahmanyasser101112@gmail.com>
2023-08-21 01:01:56 +0300

    implemented the new features to the Sniper

--- sniper.cpp
+++ sniper.cpp
@@ -7,18 +7,32 @@ Sniper :: Sniper()
     Set_Soldier_Damage_Value();
 }
 
-void Commando :: Set_Soldier_Name()
+void Sniper :: Set_Soldier_Name()
 {
     cout << "Sniper's Name: ";
     cin >> Soldier_Name;
 }
 
-void Commando :: Set_Soldier_Special_Ability()
+void Sniper :: Set_Soldier_Special_Ability()
 {
-    Soldier_Special_Ability = "carrying out raids and operating in small teams behind enemy lines";
+    Soldier_Special_Ability = "able to eliminate lightly armored targets with one shot and inflict severe damage to heavily armored targets";
 }
     
-void Commando :: Set_Soldier_Damage_Value()
+void Sniper :: Set_Soldier_Damage_Value()
 {
     Soldier_Damage_Value = 90;        
+}
+void Sniper :: get_Soldier_Name()
+{
+    return Soldier_Name;
+}
+
+void Sniper :: get_soldier_Special_Ability()
+{
+    return Soldier_Special_Ability;
+}
+
+void Sniper :: get_soldier_Damage_Value()
+{
+    return Soldier_Damage_Value;
 }
