8b251621cc5763a7e31f225aacfec610311026a9
abdelrahmanyasser10 <abdelrahmanyasser101112@gmail.com>
2023-08-20 23:27:27 +0300

    Implemented each method of a soldier in a the Commando Soldier

---
+++ commando.cpp
@@ -0,0 +1,16 @@
+Commando(){
+    void Commando :: Set_Soldier_Name(){
+        cout << "Commando's Name: ";
+        cin >> Soldier_Name;
+    }
+
+    void Commando :: Set_Soldier_Special_Ability()
+    {
+        Soldier_Special_Ability = "carrying out raids and operating in small teams behind enemy lines";
+    }
+    
+    void Commando :: Set_Soldier_Damage_Value()
+    {
+        Soldier_Damage_Value = 85;        
+    }
+}
