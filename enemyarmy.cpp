aa2edc41807bf91838591b2040ef2641edb5a087
noor <noorha728@gmail.com>
2023-08-22 00:41:13 +0200

    two methods decleration are done

---
+++ enemyarmy.cpp
@@ -0,0 +1,9 @@
+#include "enemyarmy.h"
+
+void enemyarmy::attack(){
+cout<<"the enemy is attacking us";
+}
+void enemyarmy::defend(){
+    cout <<"the enemy is defending for themselves";
+}
+bool enemyarmy::
