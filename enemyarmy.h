8c550e8781dbd0530c46af3b75b05d1302a52766
noor <noorha728@gmail.com>
2023-08-22 00:26:52 +0200

    enemy army modes created

---
+++ enemyarmy.h
@@ -0,0 +1,13 @@
+#ifndef _ENEMYARMY_H_
+#define _ENEMYARMY_H_
+#include <iostream>
+#include <string>
+
+using namespace std;
+class enemyarmy {
+public:
+    void attack();
+    void defend();
+
+};
+#endif _ENEMYARMY_H_
