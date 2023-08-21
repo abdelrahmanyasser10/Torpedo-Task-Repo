b30ee6a1a52fdf7af3eae09ee64ca39930f4ac8d
noor <noorha728@gmail.com>
2023-08-22 00:43:22 +0200

    the defintions of methods are done

--- enemyarmy.cpp
+++ enemyarmy.cpp
@@ -1,9 +1,17 @@
 #include "enemyarmy.h"
-
+bool attackk = false;
+bool defendd = false;
 void enemyarmy::attack(){
 cout<<"the enemy is attacking us";
+attackk = true;
 }
 void enemyarmy::defend(){
     cout <<"the enemy is defending for themselves";
+    defendd = true;
 }
-bool enemyarmy::
+bool enemyarmy::isAttack(){
+    return attackk;
+}
+bool enemyarmy::isDefend(){
+    return defendd;
+}
