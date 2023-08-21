578702b59eb2c9592c7c9f541a9084d1f035e062
noor <noorha728@gmail.com>
2023-08-21 20:39:31 +0200

    soliders and vehicles command classes are created

---
+++ command.h
@@ -0,0 +1,27 @@
+#ifndef _COMMANDS_H_
+#define _COMMANDS_H_
+
+
+#include<iostream>
+#include<string>
+
+
+using namespace std;
+
+class commands_soliders:protected Soldiers {
+    
+public:
+    commands_soliders();
+    void attackSoliders();
+     void defendSoliders();
+     void report();
+     
+
+};
+class commands_vehicle : protected vehicles{
+commands_vehicle();
+void fireVehicles();
+     void moveVehicles();
+      void report();
+};
+#endif  _COMMANDS_H_
