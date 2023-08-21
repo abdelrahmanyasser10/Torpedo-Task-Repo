50c4bbccf4daa2cd3c1084cd67fad42d6aa50774
noor <noorha728@gmail.com>
2023-08-21 21:03:06 +0200

    remove the constructor because its uncessery

--- command.h
+++ command.h
@@ -5,22 +5,22 @@
 #include<iostream>
 #include<string>
 #include "vehicles.h"
-#include "soliders.h"
+#include "soldiers.h"
 
 using namespace std;
 
-class commands_soliders:protected Soldiers {
-    
+class commands_soliders:protected Soldier {
+
 public:
-    commands_soliders();
+
     void attackSoliders();
      void defendSoliders();
      void report();
-     
+
 
 };
 class commands_vehicle : protected vehicles{
-commands_vehicle();
+     public:
 void fireVehicles();
      void moveVehicles();
       void report();
